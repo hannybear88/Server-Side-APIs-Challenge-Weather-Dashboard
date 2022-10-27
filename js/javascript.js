57cebe766230a3e70b4ab718f192e02ee247b23b
hannybear88 <hannahkchung88@gmail.com>
2022-10-27 00:37:00 -0700

    started on README

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+var HKCAPIKry = "3f1cab551e8d4185d8486ab763d9b25b";
+
+var city;
+var state;
+var countrycode;
+// var queryURL = https://api.openweathermap.org/data/2.5/weather?q= + city + state code + country code + "&appid=" + HKCAPIKey;
+// {city name},{state code},{country code}&appid={API key}
+// fetch(queryURL)
+
+// api.openweathermap.org/data/2.5/forecast/daily?q={city name},{state code},{country code}&cnt={cnt}&appid={API key}
